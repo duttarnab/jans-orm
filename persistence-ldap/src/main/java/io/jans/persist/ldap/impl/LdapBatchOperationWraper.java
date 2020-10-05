4fa56878e9d084ce769635f3e5bb7f0177d05e05
YuriyZ <yzabrovarniy@gmail.com>
2020-10-05 19:18:59 +0300

    Files missed during refactoring.

---
+++ persistence-ldap/src/main/java/io/jans/persist/ldap/impl/LdapBatchOperationWraper.java
@@ -0,0 +1,50 @@
+package io.jans.persist.ldap.impl;
+
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import io.jans.persist.model.BatchOperation;
+import io.jans.persist.reflect.property.PropertyAnnotation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LDAP batch operation wrapper
+ *
+ * @author Yuriy Movchan Date: 02/07/2010
+ */
+public class LdapBatchOperationWraper<T> {
+
+    private LdapEntryManager ldapEntryManager;
+    private Class<T> entryClass;
+    private List<PropertyAnnotation> propertiesAnnotations;
+
+    private BatchOperation<T> batchOperation;
+
+    public LdapBatchOperationWraper(BatchOperation<T> batchOperation) {
+        this.batchOperation = batchOperation;
+    }
+
+    public LdapBatchOperationWraper(BatchOperation<T> batchOperation, LdapEntryManager ldapEntryManager, Class<T> entryClass,
+            List<PropertyAnnotation> propertiesAnnotations) {
+        this.batchOperation = batchOperation;
+        this.ldapEntryManager = ldapEntryManager;
+        this.entryClass = entryClass;
+        this.propertiesAnnotations = propertiesAnnotations;
+    }
+
+    public final BatchOperation<T> getBatchOperation() {
+        return batchOperation;
+    }
+
+    public List<T> createEntities(SearchResult searchResult) {
+        if (ldapEntryManager == null) {
+            return new ArrayList<T>(0);
+        }
+        SearchResultEntry[] searchResultEntry = searchResult.getSearchEntries()
+                .toArray(new SearchResultEntry[searchResult.getSearchEntries().size()]);
+
+        return ldapEntryManager.createEntities(entryClass, propertiesAnnotations, searchResultEntry);
+    }
+
+}
